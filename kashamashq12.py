68ea62e24960c496529586394971374656fcbde3
xushxabar <139157421+xushxabar@users.noreply.github.com>
2023-08-01 23:51:27 +0500

    Add files via upload

---
+++ kashamashq12.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  1 17:39:52 2023
+
+@author: User
+"""
+
+taom={'ali':'osh',
+      'karim':'makaron',
+      'salim':'shokolad',
+      'qobil':'lapsha'}
+#print('Bizda Ali,Karim,Salim va Qobilning sevimli taomi ma\'lumoti bor')
+for d in range(4):
+    ism=input('kimni sevimli taomini bilmoqchisiz?\n')
+    kasha=taom.get(ism)
+    if kasha==None:
+         print(f'Bunda {ism} ismi yo\'q')
+    else:  
+        print(f"{ism.title()}ning sevimli taomi {taom[ism]}")
+       
