047062be6bb74d3ed85f7c0f054debc981b1c111
xushxabar <xushxabarmi@gmail.com>
2023-08-02 23:40:22 +0500

    lug'at bilan ishlashdan mashqlar
    lug'at bilan ishlashdan mashqlar

---
+++ mashq11.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  1 17:24:45 2023
+
+@author: User
+"""
+
+ota={}
+ota["ism"]='mavluddin'
+ota['yil']='1954'
+ota['joy']='samarqand'
+#print(ota)
+print(f"Otasining ismi {ota['ism'].title()}, {ota['yil']}-yilda, {ota['joy'].title()} viloyatida tug'ilgan.")
